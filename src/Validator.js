321d833262415f328ea387f0f801774a298398ce
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-24 15:56:23 -0400

    adding states and if/else

--- src/Validator.js
+++ src/Validator.js
@@ -2,6 +2,48 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      valid: true
+    }
+  }
+
+  handleEmail(event) {
+    this.setState({
+      email.event.target.value
+    })
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password.event.target.value
+    })
+  }
+
+  handleConfirmPassword(event){
+    this.setState({
+      confirmPassword.event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.checkMatch()
+  }
+
+  checkMatch() {
+    if (this.state.password === this.state.confirmPassword) {
+      this.setState({valid: true})
+    } else {
+      this.setState({valid: false})
+
+    }
+  }
+
   render() {
     return (
       <div className="form">
